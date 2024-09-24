27155a6213ef98f43dddeaf6157c738fcb9bd523
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2024-09-24 10:25:06 -0300

    Criando projeto

---
+++ Historia/Assets/Script/MenuPrincipal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public GameObject painelOpçao;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
