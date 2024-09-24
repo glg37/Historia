68e33ed0bddd9b3f1214da9bc15ea0e175bbaf8f
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2024-09-24 10:33:04 -0300

    Codigo Menu

--- Historia/Assets/Script/MenuPrincipal.cs
+++ Historia/Assets/Script/MenuPrincipal.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuPrincipal : MonoBehaviour
 {
-    public GameObject painelOpçao;
+    [SerializeField] private string NomeDaFase;
+    [SerializeField] private GameObject painelOpçao;
+    [SerializeField] private GameObject painelMenuPrincipal;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +18,24 @@ public class MenuPrincipal : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Jogar()
+    {
+        SceneManager.LoadScene(NomeDaFase);
+    }
+    public void AbrirOpçao()
+    {
+        painelOpçao.SetActive(true);
+        painelMenuPrincipal.SetActive(false);
+    }
+    public void FecharOpçao()
+    {
+        painelOpçao.SetActive(false);
+        painelMenuPrincipal.SetActive(true);
+    }
+    
+    public void Sair()
+    {
+        Application.Quit();
     }
 }
