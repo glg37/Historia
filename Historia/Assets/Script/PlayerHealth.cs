6f2bacff18bc01dae2c7b15d0b2f8f58393d45a6
Victor Ferreira <victor13.09.08ferreira@gmail.com>
2024-10-03 14:35:54 -0300

    Fazendo a Ui seguir o player

--- Historia/Assets/Script/PlayerHealth.cs
+++ Historia/Assets/Script/PlayerHealth.cs
@@ -7,21 +7,35 @@ public class PlayerHealth : MonoBehaviour
     public float maxHealth = 100f;
     private float currentHealth;
     public Slider healthBar;
+    public Vector3 healthBarOffset = new Vector3(0, 2, 0); 
+    public Camera mainCamera;
 
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.maxValue = maxHealth;
         healthBar.value = currentHealth;
+
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
     }
 
-  
+    void Update()
+    {
+       
+        Vector3 worldPosition = transform.position + healthBarOffset;
+        Vector3 screenPosition = mainCamera.WorldToScreenPoint(worldPosition);
+
+        healthBar.transform.position = screenPosition;
+    }
+
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         Debug.Log("Player tomou " + damage + " de dano.");
 
-       
         healthBar.value = currentHealth;
 
         if (currentHealth <= 0)
@@ -32,9 +46,7 @@ public class PlayerHealth : MonoBehaviour
 
     void Die()
     {
-      
         Debug.Log("Player morreu.");
-       
         Destroy(gameObject);
     }
 }
