b3a86f3baf4cbf8a2bd5c9743b650a08bf0368dc
Victor Ferreira <victor13.09.08ferreira@gmail.com>
2024-10-04 09:09:39 -0300

    Agora o Enemy tem vida Ui

--- Historia/Assets/Script/EnemyHealth.cs
+++ Historia/Assets/Script/EnemyHealth.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EnemyHealth : MonoBehaviour
 {
-    public float maxHealth = 50f;
+    public float maxHealth = 100f;
     private float currentHealth;
+    public Slider healthBar;
+    public Vector3 healthBarOffset = new Vector3(0, 2, 0);
+    public Camera mainCamera;
+
 
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
+
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    void Update()
+    {
+
+        Vector3 worldPosition = transform.position + healthBarOffset;
+        Vector3 screenPosition = mainCamera.WorldToScreenPoint(worldPosition);
+
+        healthBar.transform.position = screenPosition;
     }
 
     public void TakeDamage(float damage)
@@ -17,15 +37,17 @@ public class EnemyHealth : MonoBehaviour
         currentHealth -= damage;
         Debug.Log("Inimigo tomou " + damage + " de dano.");
 
+        healthBar.value = currentHealth;
+
         if (currentHealth <= 0)
         {
             Die();
         }
     }
 
+
     void Die()
     {
-       
         Debug.Log(" morreu.");
         Destroy(gameObject);
     }
