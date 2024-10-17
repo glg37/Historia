c3a500d4f3917ade2cac0299d531171816ec2c69
VictorFerreira <victor13.09.08ferreira@gmail.com>
2024-10-17 09:32:06 -0300

    fazendo o spawn enemy

---
+++ Historia/Assets/Script/SpawnEnemy.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject miniBossPrefab;
+    public float spawnRate;
+    float timer;
+    public Transform[] spawnPoints;
+    public List<GameObject> enemies;
+    public int maxEnemy = 5;
+    private int totalSpawnedEnemies = 0;
+    private bool bossSpawned = false;
+
+    void Start()
+    {
+        StartCoroutine(TesTe());
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnRate && totalSpawnedEnemies < maxEnemy && !bossSpawned)
+        {
+            Spawn();
+            timer = 0;
+        }
+        
+    }
+    void Spawn()
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                enemies.Add(Instantiate(enemyPrefab, spawnPoints[i].position, spawnPoints[i].rotation));
+                totalSpawnedEnemies++;
+            }
+        }
+    }
+    void Spawn(int qtd)
+    {
+        for (int i = 0; i < qtd; i++)
+        {
+            if (totalSpawnedEnemies < maxEnemy)
+            {
+                int random = Random.Range(0, spawnPoints.Length);
+                enemies.Add(Instantiate(enemyPrefab, spawnPoints[random].position, spawnPoints[random].rotation));
+                totalSpawnedEnemies++;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+   
+
+    IEnumerator TesTe()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
