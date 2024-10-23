e9c1ca0f4d4574c4920f838242ab83d66debe729
VictorFerreira <victor13.09.08ferreira@gmail.com>
2024-10-23 11:39:34 -0300

    cenario

--- Historia/Assets/Script/Enemy.cs
+++ Historia/Assets/Script/Enemy.cs
@@ -5,16 +5,19 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class Enemy : MonoBehaviour
 {
-    [Header("Mecanicas Enemy")]
-
+    [Header("Mecânicas Enemy")]
     public float speed = 3f;
     public GameObject sword;
+    public LayerMask playerLayer;  
+    public float visionRange = 5f;
+    public float attackRange = 2f; 
+
+
     private Transform player;
-
-
     private Rigidbody2D rb;
-    
     private bool canMove = true;
+    private bool isPatrolling = true;
+    private bool isFacingRight = true;
 
     void Start()
     {
@@ -24,19 +27,70 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        if (canMove && player != null)
+        if (player != null)
+        {
+            if (IsPlayerInSight())
+            {
+                
+                canMove = true;
+                isPatrolling = false;
+                MoveTowardsPlayer();
+            }
+            else
+            {
+                
+                isPatrolling = true;
+                Patrol();
+            }
+
+            if (Vector2.Distance(transform.position, player.position) < attackRange && canMove)
+            {
+                Attack();
+            }
+        }
+    }
+
+    bool IsPlayerInSight()
+    {
+        
+        Vector2 direction = isFacingRight ? Vector2.right : Vector2.left;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, visionRange, playerLayer);
+        return hit.collider != null && hit.collider.CompareTag("Player");
+    }
+
+    void MoveTowardsPlayer()
+    {
+        if (canMove)
         {
             Vector2 direction = (player.position - transform.position).normalized;
             rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
         }
-        else
-        {
-            rb.velocity = Vector2.zero;
-        }
+    }
 
-        if (Vector2.Distance(transform.position, player.position) < 2f && canMove)
+    void Patrol()
+    {
+        if (isPatrolling)
         {
-            Attack();
+            
+            rb.velocity = new Vector2((isFacingRight ? speed : -speed), rb.velocity.y);
+        }
+    }
+
+    void Attack()
+    {
+        sword.GetComponent<Sword>().Swing(gameObject);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+           
+            isFacingRight = !isFacingRight;
+            
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1; 
+            transform.localScale = localScale;
         }
     }
 
@@ -45,18 +99,11 @@ public class Enemy : MonoBehaviour
         canMove = value;
     }
 
-    void Attack()
+    void OnDrawGizmosSelected()
     {
-        sword.GetComponent<Sword>().Swing(gameObject);
-    }
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Parede"))
-        {
-            transform.Rotate(0, 180, 0); 
-            
-            
-
-        }
+      
+        Gizmos.color = Color.red;
+        Vector2 direction = isFacingRight ? Vector2.right : Vector2.left;
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + direction * visionRange);
     }
 }
--- Historia/Assets/Script/Npc.cs
+++ Historia/Assets/Script/Npc.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Npc : MonoBehaviour
 {
+    [Header("Dialgo Npc")]
     public float interactionRange = 3f;
-    public GameObject dialogueUI; 
-    public string npcDialogue = "Nao e aqui capitao,volte e tomine o territorio ";
+    public GameObject dialogueUI;
+    public string npcDialogue = "Não é por aqui, capitão. Volte e mate os muçulmanos e domine os territórios deles no Egito.";
+
 
     private Transform player;
-
+    public Button exitButton;
+    private Player player2;
     void Start()
     {
+        player2 = FindObjectOfType<Player>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        dialogueUI.SetActive(false); 
+        dialogueUI.SetActive(false);
+        exitButton.onClick.AddListener(ExitNpc);
     }
 
     void Update()
@@ -30,12 +36,28 @@ public class Npc : MonoBehaviour
         else
         {
             dialogueUI.SetActive(false);
+            exitButton.gameObject.SetActive(false);
         }
     }
 
     void AbrirDialogo()
     {
         dialogueUI.SetActive(true);
-        dialogueUI.GetComponentInChildren<UnityEngine.UI.Text>().text = npcDialogue; 
+        
+        dialogueUI.GetComponentInChildren<UnityEngine.UI.Text>().text = npcDialogue;
+        exitButton.gameObject.SetActive(true);
+        player2.SetCanMove(false);
+    }
+    public void ExitNpc()
+    {
+       EndDialogo();
+    }
+    public void EndDialogo()
+    {
+        player2.SetCanMove(true);
+        dialogueUI.SetActive(false);
+       
+        exitButton.gameObject.SetActive(false);
+        
     }
 }
