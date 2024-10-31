a7a93c7ac5fb25d5d7fa332061aeb1dfc815c969
VictorFerreira <victor13.09.08ferreira@gmail.com>
2024-10-31 16:01:17 -0300

    Jogo termino

--- Historia/Assets/Script/Termino.cs
+++ Historia/Assets/Script/Termino.cs
@@ -6,13 +6,20 @@ public class Termino : MonoBehaviour
 {
     public void Jogar3()
     {
+        StartCoroutine(fuego());
+        Time.timeScale = 1;
+    }
+
+    IEnumerator fuego()
+    {
+        yield return new WaitForSeconds(8);
+        SceneManager.LoadScene("Jogo");
+    }
+    public void Jogar6()
+    {
         SceneManager.LoadScene("Jogo");
         Time.timeScale = 1;
     }
-    IEnumerator fuego()
-    {
-        yield return new WaitForSeconds(17);
-        SceneManager.LoadScene("Jogo");
-    }
-
+   
+    
 }
